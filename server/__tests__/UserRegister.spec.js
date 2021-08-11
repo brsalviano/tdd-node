ab096142903781c8f18fd7b32a4310aaa7951344
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-08-11 14:33:20 -0300

    Criação de usuário - TDD red

--- server/__tests__/UserRegister.spec.js
+++ server/__tests__/UserRegister.spec.js
@@ -1,5 +1,12 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+beforeAll(() => {
+  //Inicializa o banco...
+  return sequelize.sync();
+});
 
 describe('User Registration', () => {
   it('returns 200 OK when signup request is valid', (done) => {
@@ -27,6 +34,23 @@ describe('User Registration', () => {
       .then((response) => {
         expect(response.body.message).toBe('User created');
         done();
+      });
+  });
+
+  it('saves the user to database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        //Fazer uma query na tabela de usuários.
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+          done();
+        });
       });
   });
 });
