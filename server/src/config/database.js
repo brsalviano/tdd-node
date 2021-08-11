90c92d520eda4a240594a62d5d95088fb8f1f09a
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-08-11 14:17:06 -0300

    Configuração inicial do sequelize e criação do primeiro model

---
+++ server/src/config/database.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+//1º parâmetro - Nome do banco de dados
+//2º parâmetro - Nome do usuário do banco
+//3º parametro - senha
+//4º parametro - configurações do banco. No caso especificamos o dialeto que vamos usar e
+//como vamos usar sqlite estamos informando onde será o armazenamento, que será o arquivo chamado database.sqlite.
+const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-p4ss', {
+  dialect: 'sqlite',
+  storage: './database.sqlite',
+});
+
+module.exports = sequelize;
