2971a48c11ef69938d8250a9f41a6c91cbe4aae2
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-08-11 20:52:58 -0300

    Arquivo com variáveis de configuração e especificação de ambiente

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,14 +1,12 @@
 const Sequelize = require('sequelize');
+const config = require('config');
 
-//1º parâmetro - Nome do banco de dados
-//2º parâmetro - Nome do usuário do banco
-//3º parametro - senha
-//4º parametro - configurações do banco. No caso especificamos o dialeto que vamos usar e
-//como vamos usar sqlite estamos informando onde será o armazenamento, que será o arquivo chamado database.sqlite.
-const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-p4ss', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false,
+const dbConfig = config.get('database');
+
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  dialect: dbConfig.dialect,
+  storage: dbConfig.storage,
+  logging: dbConfig.logging,
 });
 
 module.exports = sequelize;
