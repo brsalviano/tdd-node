3de2066ab8445d690d74daecd8ebf16bcb2b76e3
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-08-17 10:20:46 -0300

    Validação de username com min 4 e max 32 caracteres

--- server/src/user/UserRouter.js
+++ server/src/user/UserRouter.js
@@ -6,7 +6,12 @@ const router = express.Router();
 
 router.post(
   '/api/1.0/users',
-  check('username').notEmpty().withMessage('Username cannot be null'),
+  check('username')
+    .notEmpty()
+    .withMessage('Username cannot be null')
+    .bail() //Já para de verificar as próximas validações se a anterior já deu erro
+    .isLength({ min: 4 })
+    .withMessage('Must have min 4 and max 32 characters'),
   check('email').notEmpty().withMessage('E-mail cannot be null'),
   check('password').notEmpty().withMessage('Password cannot be null'),
   async (req, res) => {
